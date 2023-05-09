d7a821342caf6552138d71280a537acd240fefd9
Valerio Salzano <vlr.slzn@gmail.com>
2023-05-09 16:11:29 +0200

    added some style with bootstrap

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,32 +5,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>About us</title>
 </head>
 
-<body>
-    <header>
-        <h1>
+<body class="text-bg-dark">
+    <header class=" text-bg-light text-center py-3">
+        <h1 class="display-2 mb-2">
             {{ $title }}
         </h1>
 
-        <nav>
-            <ul>
-                @foreach ($nav_links as $key => $nav_link)
-                    <li>
-                        <a href="{{ $nav_link }}">{{ $key }}</a>
-                    </li>
-                @endforeach
-            </ul>
-        </nav>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+					<div class="container">
+							<ul class="navbar-nav me-auto text-capitalize">
+									@foreach ($nav_links as $key => $nav_link)
+											<li class="nav-item">
+													<a class="nav-link" href="{{ $nav_link }}">{{ $key }}</a>
+											</li>
+									@endforeach
+							</ul>
+					</div>
+			</nav>
 
     </header>
 
-    <main>
-
+    <main class="container py-5">
         <section>
-            <h2> {{ $section_title }} </h2>
-            <p> {{ $text }} </p>
+            <h2 class="py-3 text-center display-6 fw-semibold"> {{ $section_title }} </h2>
+            <p class="text-center my-3 lh-lg"> {{ $text }} </p>
             <small> {{ $caption }} </small>
         </section>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,37 +5,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Laravel: primi passi</title>
 </head>
 
-<body>
-    <header>
-        <h1>
+<body class="text-bg-dark">
+    <header class=" text-bg-light text-center py-3">
+        <h1 class="display-2 mb-2">
             {{ $title }}
         </h1>
-        <h3>
+        <h3 class="display-6 mb-1">
             {{ $subtitle }}
         </h3>
-        <nav>
-            <ul>
-                @foreach ($nav_links as $key => $nav_link)
-                    <li>
-                        <a href="{{ $nav_link }}">{{ $key }}</a>
-                    </li>
-                @endforeach
-            </ul>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container">
+                <ul class="navbar-nav me-auto text-capitalize">
+                    @foreach ($nav_links as $key => $nav_link)
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ $nav_link }}">{{ $key }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
         </nav>
     </header>
 
-    <main>
-        <p>
+    <main class="container py-5">
+        <p class="text-center my-4 fs-3">
             {{ $message }}
         </p>
 
-        <h5>Here's my todo list</h5>
-        <ol>
+        <h5 class="py-3 text-center">Here's my todo list</h5>
+
+        <ol class="list-group">
             @foreach ($todos as $todo)
-                <li>
+                <li class="list-group-item">
 
                     @if ($todo['done'])
                         &#9745;
