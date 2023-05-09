e09d559ed78244e4cd596b68ff125210be98dc12
Valerio Salzano <vlr.slzn@gmail.com>
2023-05-09 14:30:13 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel: primi passi</title>
+</head>
+
+<body>
+  <header>
+    <h1>Laravel</h1>
+    <h3>Primi passi</h3>
+  </header>
+
+</body>
+
+</html>
